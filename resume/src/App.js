d3d25940fb3d595e2ea1eecceab5247d2b01b9ab
DENIS-DONE-PROJECT <denis.avocat59@gmail.com>
2023-06-13 21:40:17 +0200

    FIX main grid

--- resume/src/App.js
+++ resume/src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 function App() {
     return (
         <div className="main">
-            <div className="one">Picture</div>
-            <div className="two">Profil</div>
-            <div className="three">trois</div>
-            <div className="four">quatre</div>
-            <div className="five">cinq</div>
+            <div className="one">1 Picture</div>
+            <div className="two">2 Profil</div>
+            <div className="Left"></div>
+
+            <div className="three">3 Identity</div>
+            <div className="four">4 EXP</div>
+            <div className="five">5 HardSkill</div>
             <div className="seven">sept</div>
             <div className="height">huit</div>
             <div className="nine">neuf</div>
