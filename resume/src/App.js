642c083e88417de3d7945ffa9bcf42e80ab23398
DENIS-DONE-PROJECT <denis.avocat59@gmail.com>
2023-06-13 16:09:26 +0200

    IMPROVE App

--- resume/src/App.js
+++ resume/src/App.js
@@ -3,30 +3,15 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="container">
-            <div className="gridLeft">
-                <div className="picture">picture</div>
-                <div className="identity">identity</div>
-                <div className="hardkill">hardkills`</div>
-                <div className="Language">Language</div>
-                <div className="softskill">softskill</div>
-            </div>
-            <div className="gridRight">
-                <div className="profile">profile</div>
-                <div className="gridExperience">
-                    experience
-                    <div className="childExperience1">Exp1</div>
-                    <div className="childExperience2">Exp2</div>
-                    <div className="childExperience3">Exp3</div>
-                </div>
-
-                <div className="gridTraining">
-                    Training
-                    <div className="childTaining1">Training 1</div>
-                    <div className="childTaining2">Training 2</div>
-                    <div className="childTaining3">Training 3</div>
-                </div>
-            </div>
+        <div className="main">
+            <div className="one">Picture</div>
+            <div className="two">Profil</div>
+            <div className="three">trois</div>
+            <div className="four">quatre</div>
+            <div className="five">cinq</div>
+            <div className="seven">sept</div>
+            <div className="height">huit</div>
+            <div className="nine">neuf</div>
         </div>
     );
 }
